fe0405dc632ecb065e401154cc575e3d1faa4ddc
Roberto <robertopescee@hotmail.com>
2017-05-20 14:12:33 -0700

    finishing email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,6 @@
 class Email < ApplicationRecord
-	validates :name, :email, presence: true
-	validates :email, uniqueness: { case_sensitive: false }
+	validates :name, length: { minimum: 3, maximum: 25 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { minimum: 7, maximum: 80 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 
 end
